688e2a2e76bf54d298d2c078ddf11ea5847b226f
Kamal Nayan <geniusamansingh@gmail.com>
2022-12-18 13:15:45 +0000

    positive pic added

--- sessions/src/assets/cardPictures/index.ts
+++ sessions/src/assets/cardPictures/index.ts
@@ -4,4 +4,5 @@ export { default as Card3 } from './Card3.jpg';
 export { default as Card4 } from './Card4.jpg';
 export { default as Card5 } from './Card5.jpg';
 export { default as Card6 } from './Card6.jpg';
-export { default as Card7 } from './Card7.jpg';
+export { default as Meditating } from './meditating.jpg';
+
