02e6f9993124418b976e11fa40d7994586f3f729
Kamal Nayan <geniusamansingh@gmail.com>
2022-12-18 13:39:55 +0000

    chat + doc sessions added

--- sessions/src/assets/cardPictures/index.ts
+++ sessions/src/assets/cardPictures/index.ts
@@ -1,8 +1,9 @@
 export { default as Card1 } from './Card1.jpg';
-export { default as Card2 } from './Card2.jpg';
+export { default as Bhagvat } from './bhagvat.jpg';
 export { default as Card3 } from './Card3.jpg';
 export { default as Card4 } from './Card4.jpg';
 export { default as Card5 } from './Card5.jpg';
-export { default as Card6 } from './Card6.jpg';
+export { default as Chakras } from './chakras.jpg';
 export { default as Meditating } from './meditating.jpg';
-
+export { default as Truth } from './truth.jpg';
+export { default as User } from './user.jpg';
